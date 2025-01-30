9511ddcc1607b92f91bc7cd14eef9d4e88e7e140
NACH07788 <nachocarp7788@gmail.com>
2025-01-30 19:06:05 -0300

    detalles

--- HE/Pages/JS/SCRIPT.js
+++ HE/Pages/JS/SCRIPT.js
@@ -3,25 +3,29 @@ let trabajos = [
     "id": 1111,
     "tittle": "Macetero B-1",
     "price": 2999,
-    "description": "lorem"
+    "description": "lorem",
+    "imagen":"../CSS/Productos/MACETAS/MACETERO_B1.webp"
   },
   {
     "id": 3333,
-    "tittle": "Macetero B-3",
+    "tittle": "Macetero B-2",
     "price": 3999,
-    "description": "lorem"
+    "description": "lorem",
+    "imagen":"../CSS/Productos/MACETAS/MACETERO_B2.jpeg"
   },
   {
     "id": 4444,
-    "tittle": "Macetero B-4",
+    "tittle": "Macetero B-3",
     "price": 7999,
-    "description": "lorem"
+    "description": "lorem",
+    "imagen":"../CSS/Productos/MACETAS/MACETERO_B3.jpg"
   },
   {
     "id": 5555,
-    "tittle": "Macetero B-5",
+    "tittle": "Macetero B-4",
     "price": 5999,
-    "description": "lorem"
+    "description": "lorem",
+    "imagen": "../CSS/Productos/MACETAS/MACETERO_B4.jpeg"
   }
 ];
 
@@ -35,19 +39,19 @@ function showWorks() {
 // Modificar DOM
 let trabajosContainer = document.getElementById('Trabajos');
 let contenedor = document.createElement('div');
-contenedor.className = 'row';
+contenedor.className = 'row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3';
 
 function armarCartas() {
   trabajos.forEach(trabajo => {
     let cartaBootStrap = document.createElement('div');
+    cartaBootStrap.className = "col-lg-4 col-md-2 mb-4";
     cartaBootStrap.innerHTML =
-      `<div class="col-lg-4 col-md-6 mb-4">
-          <div class="card">
+      `   <div class="card">
             <div class="bg-image hover-zoom ripple" data-mdb-ripple-color="light">
-              <img src="#" alt="${trabajo.tittle}" />
+              <img src="${trabajo.imagen}" alt="${trabajo.tittle}" />
             </div>
             <div class="card-body">
-              <a href="" class="text-reset">
+              <a href="" class="text-center">
                 <h5 class="card-title mb-3">${trabajo.tittle}</h5>
               </a>
               <div>
@@ -59,11 +63,11 @@ function armarCartas() {
               </h6>
             </div>
           </div>
-       </div>`;
+       `;
     contenedor.append(cartaBootStrap);
   });
   trabajosContainer.append(contenedor);
-  
+
   document.querySelectorAll('.btn-descripcion').forEach(button => {
     button.addEventListener('click', (e) => {
       const descriptionText = e.target.nextElementSibling;
